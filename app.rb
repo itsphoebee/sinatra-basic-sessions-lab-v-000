c511b2c0d34617dd95f53f0f4b68c78400723d0f
Phoebe Lau <phoebelau7@gmail.com>
2018-01-12 02:59:51 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,7 +1,11 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-
+  configure do
+    enable :sessions
+    set :sessions_secret, "supersecretstuff"
+  end
+  
   get '/' do
     erb :index
   end
